bc6ba08c42e2983b3a966d463dd65ffcb7033a4f
dani0105 <danielrojas0105@gmail.com>
2022-01-18 23:36:53 -0600

    Agrega la imagen de perfile

--- src/app/components/layout/profile/profile.component.tsx
+++ src/app/components/layout/profile/profile.component.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
-import {i18n} from '../../../i18n/i18n';
+import { i18n } from '../../../i18n/i18n';
+import profileImage from './profile.jpg';
+import { ProfileImage } from './profile.styles';
 
 export class ProfileComponent extends React.Component {
 
@@ -14,15 +16,18 @@ export class ProfileComponent extends React.Component {
         </Row>
         <Row className="mt-4">
           <Col md="9">
-            <p className="text text-start">
-              {i18n.profile.description1}
-            </p>
-            <p className="text text-start">
-            {i18n.profile.description2}
-            </p>
+            <div className="align-middle h-100">
+              <p className="text text-start">
+                {i18n.profile.description1}
+              </p>
+              <p className="text text-start">
+                {i18n.profile.description2}
+              </p>
+            </div>
+
           </Col>
-          <Col md="3">
-            
+          <Col md="3" className="order-first order-sm-last">
+            <ProfileImage className="w-100" src={profileImage} />
           </Col>
         </Row>
       </Container>
--- src/app/components/layout/profile/profile.styles.ts
+++ src/app/components/layout/profile/profile.styles.ts
@@ -1,2 +1,12 @@
 import styled from "styled-components";
 
+export const ProfileImage = styled.img`
+  clip-path: circle();
+  max-width: 250px;
+  @media (max-width: 768px) {
+    width: 50%!important;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+  }
+`;
